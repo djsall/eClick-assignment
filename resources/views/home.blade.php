1d39d44ca783c4342ae2afea445fe38bc143ab7d
leventeproksa <proksalevente@gmail.com>
2023-02-07 18:07:43 +0100

    added calendar view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,22 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-8">
-				<div class="card">
-					<div class="card-header">{{ __('Dashboard') }}</div>
-
-					<div class="card-body">
-						@if (session('status'))
-							<div class="alert alert-success" role="alert">
-								{{ session('status') }}
-							</div>
-						@endif
-
-						{{ __('You are logged in!') }}
-					</div>
-				</div>
+				<div id="calendar"></div>
 			</div>
 		</div>
 	</div>
 	@push('scripts')
-		<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
+		<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.1/index.global.min.js'></script>
+		<script>
+			document.addEventListener('DOMContentLoaded', function () {
+				var calendarEl = document.getElementById('calendar');
+				var calendar = new FullCalendar.Calendar(calendarEl, {
+					initialView: 'dayGridMonth',
+					themeSystem: 'bootstrap5',
+					events: @json($events),
+				});
+				calendar.render();
+			});
+		</script>
 	@endpush
 @endsection
