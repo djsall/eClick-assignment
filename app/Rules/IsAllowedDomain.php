b939012208e6e1e1a87bbf9849c57ae3bccc5268
leventeproksa <proksalevente@gmail.com>
2023-02-07 11:31:41 +0100

    signup validation rule for eClick addresses

---
+++ app/Rules/IsAllowedDomain.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class IsAllowedDomain implements Rule {
+
+	/**
+	 * @var string[]
+	 * Allowed email domains for users signing up
+	 */
+
+	protected $allowedDomains = [
+		'eclick.hu',
+	];
+
+	/**
+	 * Create a new rule instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		//
+	}
+
+	/**
+	 * Determine if the validation rule passes.
+	 *
+	 * @param string $attribute
+	 * @param mixed $value
+	 * @return bool
+	 */
+	public function passes($attribute, $value) {
+		//
+		$domain = substr(strrchr($value, "@"), 1);
+		if (in_array($domain, $this->allowedDomains))
+			return true;
+		return false;
+	}
+
+	/**
+	 * Get the validation error message.
+	 *
+	 * @return string
+	 */
+	public function message() {
+		return 'This application only allows signing up with @eClick.hu e-mail addresses.';
+	}
+}
