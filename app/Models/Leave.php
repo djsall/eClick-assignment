64463a8845277ce1d351fa6204262ee9342ee722
leventeproksa <proksalevente@gmail.com>
2023-02-07 12:04:42 +0100

    added leave controller, model, migration

---
+++ app/Models/Leave.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Leave extends Model {
+	use HasFactory;
+
+	/**
+	 * Returns the Employee that is on leave during this time
+	 * @return HasOne
+	 */
+	public function user(): HasOne {
+		return $this->hasOne(User::class);
+	}
+
+	/**
+	 * Returns whether this Leave was granted or not
+	 * @return boolean
+	 */
+	public function isAccepted(): bool {
+		return $this->accepted;
+	}
+
+	/**
+	 * Returns if this Leave is medical
+	 * @return bool
+	 */
+	public function isMedical(): bool {
+		return $this->type == "medical";
+	}
+
+	/**
+	 * Returns if this Leave is paid leave
+	 * @return boolean
+	 */
+	public function isPaid(): bool {
+		return $this->type == "paid";
+	}
+}
---
+++ database/migrations/2023_02_07_103751_create_leaves_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('leaves', function (Blueprint $table) {
+			$table->id();
+			$table->date("start");
+			$table->date("end");
+			$table->unsignedBigInteger("employee_id")->comment("The employee that requested this leave.");
+			$table->foreign("employee_id")->references("id")->on("users");
+			$table->boolean("accepted")->comment("Whether the leave request was accepted or not.");
+			$table->unsignedBigInteger("accepted_by")->comment("The user that accepted this leave request.");
+			$table->foreign("accepted_by")->references("id")->on("users");
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('leaves');
+	}
+};
